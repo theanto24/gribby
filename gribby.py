d510612e9ff4141a24cb0c797d4abe32c1b0ca4c
Antoni <antoni.meyer@gmail.com>
2024-02-02 15:16:22 -0700

    Started adding arguments

--- gribby.py
+++ gribby.py
@@ -1,19 +1,28 @@
 #!/bin/python3
-# This script will create the url necessary to download .grib files from NOAA to
+# This script will create the url necessary to download .grb files from NOAA to
 # use with chart plotters.
-# This script is loosly based of the get_gfs.pl script found on NOAA's website
-import datetime
+# This script is loosly based of the get_gfs.pl script found
+# here: https://para.nomads.ncep.noaa.gov/txt_descriptions/fast_downloading_grib.shtml
+from datetime import date
+import argparse
 
 #  Constants
 BASE_URL="https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/"
 #gfs.$YYYY$MM$DD/$HH/atmos/gfs.t${HH}z.pgrb2.0p25.f${FHR3}"
 
-def GetDateString(date: datetime.date):
-    _date_str = date.strftime("%Y%m%d")
+def ParseArguments():
+    ArgParser = argparse.ArgumentParser(prog="gribby", description="Download grb files for chart plottes from NOAA.")
+    ArgParser.add_argument('-s', '--startdate', type=str, help="Start date in YYYY-MM-DD format.")
+    ArgParser.add_argument('-H', '--hour', type=str, choices=['00', '06', '12', '18'], help="Start hour for forecast.")
+    ArgParser.parse_args()
+
+def GetDateString(inputDate: date):
+    _date_str = inputDate.strftime("%Y%m%d")
     return _date_str
 
 def main():
-    _date_today = datetime.date.today()
+    ParseArguments()
+    _date_today = date.today()
     print("Today's Date: ", GetDateString(_date_today))
 
 
