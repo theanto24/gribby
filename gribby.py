ed6dd2d6d4d060d80091aad7a83e379859ac2de5
Antoni <antoni.meyer@gmail.com>
2024-02-02 15:41:53 -0700

    Added default or user defined start date

--- gribby.py
+++ gribby.py
@@ -10,20 +10,37 @@ import argparse
 BASE_URL="https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/"
 #gfs.$YYYY$MM$DD/$HH/atmos/gfs.t${HH}z.pgrb2.0p25.f${FHR3}"
 
+# Global Variables
+# dateEntered : date
+
 def ParseArguments():
-    ArgParser = argparse.ArgumentParser(prog="gribby", description="Download grb files for chart plottes from NOAA.")
+    ArgParser = argparse.ArgumentParser(prog="gribby", description="Download grb files for chart plotters from NOAA.")
     ArgParser.add_argument('-s', '--startdate', type=str, help="Start date in YYYY-MM-DD format.")
     ArgParser.add_argument('-H', '--hour', type=str, choices=['00', '06', '12', '18'], help="Start hour for forecast.")
-    ArgParser.parse_args()
+    args = ArgParser.parse_args()
+
+    # print(args.startdate,args.hour)
+
+    if args.startdate is not None:
+        try:
+            _date_entered = date.fromisoformat( args.startdate )
+        except:
+            print("Invalid ISO Date. Must be in YYYY-MM-DD format.\n")
+            ArgParser.print_help()
+            exit(1)
+    else:
+        _date_entered = date.today()
+
+    return _date_entered
+
 
 def GetDateString(inputDate: date):
     _date_str = inputDate.strftime("%Y%m%d")
     return _date_str
 
 def main():
-    ParseArguments()
-    _date_today = date.today()
-    print("Today's Date: ", GetDateString(_date_today))
+    _user_date = ParseArguments()
+    print("Date: ", GetDateString(_user_date))
 
 
 if __name__ == "__main__":
