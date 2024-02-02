3c0dbf99e3b8ff11cf4227bfda4fc6fe9b1d1c5a
Antoni <antoni.meyer@gmail.com>
2024-02-02 14:27:58 -0700

    Added main script

---
+++ gribby.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# This script will create the url necessary to download .grib files from NOAA to
+# use with chart plotters.
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
+
