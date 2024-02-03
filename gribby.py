c2d1d311ea15f72450a66f00d5e275c2d5995bde
Antoni <antoni.meyer@gmail.com>
2024-02-02 18:21:18 -0700

    Added user specified or default start hour args

--- gribby.py
+++ gribby.py
@@ -3,8 +3,9 @@
 # use with chart plotters.
 # This script is loosly based of the get_gfs.pl script found
 # here: https://para.nomads.ncep.noaa.gov/txt_descriptions/fast_downloading_grib.shtml
-from datetime import date
+from datetime import date, datetime, time
 import argparse
+from math import floor
 
 #  Constants
 BASE_URL="https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/"
@@ -15,12 +16,15 @@ BASE_URL="https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/"
 
 def ParseArguments():
     ArgParser = argparse.ArgumentParser(prog="gribby", description="Download grb files for chart plotters from NOAA.")
-    ArgParser.add_argument('-s', '--startdate', type=str, help="Start date in YYYY-MM-DD format.")
-    ArgParser.add_argument('-H', '--hour', type=str, choices=['00', '06', '12', '18'], help="Start hour for forecast.")
+    ArgParser.add_argument('-s', '--startdate', type=str,
+        help="Start date in YYYY-MM-DD format. If none is specified, it will use today's date.")
+    ArgParser.add_argument('-H', '--hour', type=str, choices=['00', '06', '12', '18'],
+        help="Start hour for forecast. If none specified, it will use the current 6h forecast window.")
     args = ArgParser.parse_args()
 
     # print(args.startdate,args.hour)
 
+    # If the user entered a start date, parse it, otherwise the date to use will be today's date
     if args.startdate is not None:
         try:
             _date_entered = date.fromisoformat( args.startdate )
@@ -31,7 +35,22 @@ def ParseArguments():
     else:
         _date_entered = date.today()
 
-    return _date_entered
+    # If the user entered start hour, parse it, otherwise use the current 6h forecast window
+    if args.hour is not None:
+        _hour_entered = args.hour
+    else:
+        _current_hour = datetime.now().time().hour
+        # Amount of times the current hour is divisble by 6 so that we can use it to round down to 6h increments
+        # A block of 0 refers to 00h-06h and a block of 1 = 06h-12h etc..
+        _block = floor(_current_hour / 6)
+        _hour_entered = str.format({0:0>2}, (_block*6))
+
+
+
+
+
+
+    return _date_entered, _hour_entered
 
 
 def GetDateString(inputDate: date):
@@ -39,9 +58,9 @@ def GetDateString(inputDate: date):
     return _date_str
 
 def main():
-    _user_date = ParseArguments()
+    _user_date, _user_hour = ParseArguments()
     print("Date: ", GetDateString(_user_date))
-
+    print("Hour: ", _user_hour)
 
 if __name__ == "__main__":
     main()
